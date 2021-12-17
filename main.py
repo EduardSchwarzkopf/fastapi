2ad4134f60d9831391251ecf596d3da2430e4b34
48969167 <mail.eduard.schwarzkopf@gmail.com>
2021-12-17 16:49:07 +0100

    added pydantic

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
-
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -11,5 +19,5 @@ async def root():
 
 
 @app.post("/create")
-async def create(payload: dict = Body(...)):
+async def create(payload: Post):
     return {"message": payload}
