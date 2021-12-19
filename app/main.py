a66418fcf6c18cdbb1e1bd85dbd472775042406d
48969167 <mail.eduard.schwarzkopf@gmail.com>
2021-12-19 20:01:21 +0100

    .env added

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import users, posts, auth
+from .config import settings
 
 models.Base.metadata.create_all(bind=engine)
 
