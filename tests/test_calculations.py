5c416ccf2611859a7ab8fb989cd732bdffb64b07
48969167 <mail.eduard.schwarzkopf@gmail.com>
2021-12-23 06:38:03 +0100

    fixtures added

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,35 +1,40 @@
 import pytest
 from app.calculations import BankAccount
 
+# fixtures runs before the test
+@pytest.fixture
+def zero_bank_account():
+    return BankAccount()
 
-def test_bank_set_initial_amount():
-    bank_account = BankAccount(50)
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(50)
+
+
+def test_bank_set_initial_amount(bank_account):
 
     assert bank_account.balance == 50
 
 
-def test_bank_default_amount():
-    bank_account = BankAccount()
+def test_bank_default_amount(zero_bank_account):
 
-    assert bank_account.balance == 0
+    assert zero_bank_account.balance == 0
 
 
-def test_bank_withdraw_amount():
-    bank_account = BankAccount(50)
+def test_bank_withdraw_amount(bank_account):
     bank_account.withdraw(10)
 
     assert bank_account.balance == 40
 
 
-def test_bank_deposit_amount():
-    bank_account = BankAccount(50)
+def test_bank_deposit_amount(bank_account):
     bank_account.deposit(10)
 
     assert bank_account.balance == 60
 
 
-def test_bank_collect_interest():
-    bank_account = BankAccount(50)
+def test_bank_collect_interest(bank_account):
     bank_account.collect_interest()
 
     assert round(bank_account.balance, 2) == 55
