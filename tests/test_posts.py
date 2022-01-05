fc5606dd6df3b71813de59fe35d08b7e68a04bcc
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-05 07:49:22 +0100

    single post tests added

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -5,8 +5,6 @@ from app import schemas
 def test_get_all_posts(authorized_client, test_posts):
     res = authorized_client.get("/posts/")
 
-    print(res.json())
-
     def validate(post):
         return schemas.PostResult(**post)
 
@@ -15,3 +13,32 @@ def test_get_all_posts(authorized_client, test_posts):
 
     assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
+
+
+def test_unauthorized_user_get_one_post(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+
+    assert res.status_code == 401
+
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get("/posts/")
+
+    assert res.status_code == 401
+
+
+def test_get_one_post(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/{test_posts[0].id}")
+
+    post = schemas.Post(**res.json())
+
+    assert post.id == test_posts[0].id
+    assert post.title == test_posts[0].title
+    assert post.content == test_posts[0].content
+    assert res.status_code == 200
+
+
+def test_get_one_post_not_exits(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/9999999")
+
+    assert res.status_code == 404
