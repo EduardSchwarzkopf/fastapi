93fc3c596f7ddac0a52163bbc7c4e238db2b9a9b
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-05 07:35:39 +0100

    post test added

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,10 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.database import get_db, Base
+from app import models
 from app.main import app
 from app.config import settings
+from app.oauth2 import create_access_token
 
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_passwort}@{settings.db_host}:{settings.db_port}/{settings.db_name}_test"
@@ -48,3 +50,47 @@ def test_user(client):
     new_user = res.json()
     new_user["password"] = user_data["password"]
     return new_user
+
+
+@pytest.fixture()
+def token(test_user):
+    return create_access_token({"user_id": test_user["id"]})
+
+
+@pytest.fixture()
+def authorized_client(client, token):
+    client.headers = {**client.headers, "Authorization": "Bearer " + token}
+
+    return client
+
+
+@pytest.fixture()
+def test_posts(test_user, session):
+    posts_data = [
+        {
+            "title": "first title",
+            "content": "first content",
+            "owner_id": test_user["id"],
+        },
+        {
+            "title": "second title",
+            "content": "second content",
+            "owner_id": test_user["id"],
+        },
+        {
+            "title": "third title",
+            "content": "third content",
+            "owner_id": test_user["id"],
+        },
+    ]
+
+    def create_posts_model(post):
+        return models.Post(**post)
+
+    post_map = map(create_posts_model, posts_data)
+    posts = list(post_map)
+
+    session.add_all(posts)
+
+    session.commit()
+    return session.query(models.Post).all()
