3350509249e1a3392fdc957da3ff0d58aff0ccea
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-05 05:48:10 +0100

    added create user test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from app.main import app
+from app import schemas
 
 # use with: pytest --disable-warnings -v -x
 
@@ -11,3 +12,13 @@ def test_root():
 
     assert response.json().get("message") == "Hello, new stuff"
     assert response.status_code == 200
+
+
+def test_create_user():
+    res = client.post(
+        "/users/", json={"email": "hello123@pytest.de", "password": "password123"}
+    )
+    new_user = schemas.UserData(**res.json())
+
+    assert new_user.email == "hello123@pytest.de"
+    assert res.status_code == 201
