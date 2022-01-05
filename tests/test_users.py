6b599b372b4f2454c84a8c49fb84e006bc011667
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-05 05:59:35 +0100

    connected a test db

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,29 @@
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.database import get_db, Base
 from app.main import app
 from app import schemas
+from app.config import settings
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_passwort}@{settings.db_host}:{settings.db_port}/{settings.db_name}_test"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
 
 # use with: pytest --disable-warnings -v -x
 
