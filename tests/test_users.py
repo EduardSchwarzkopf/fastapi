f6971412bbde28882f62126d8a9df925b9d6b402
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-05 06:50:44 +0100

    added conftest for base fixtures

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,25 +1,11 @@
-import pytest
 from jose import jwt
 
 from app import schemas
 from app.config import settings
-from .database import client, session
 
 #
 # use with: pytest --disable-warnings -v -x
 #
-
-
-@pytest.fixture()
-def test_user(client):
-    user_data = {"email": "hello123@pytest.de", "password": "password123"}
-
-    res = client.post("/users/", json=user_data)
-
-    assert res.status_code == 201
-    new_user = res.json()
-    new_user["password"] = user_data["password"]
-    return new_user
 
 
 def test_create_user(client):
