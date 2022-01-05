6aa20c7410c9614f185dcc64414dc9aceee15b84
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-05 06:54:16 +0100

    added wrong login test

--- tests/test_users.py
+++ tests/test_users.py
@@ -33,3 +33,12 @@ def test_login_user(client, test_user):
     assert id == test_user["id"]
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+
+def test_invalid_login(client, test_user):
+    res = client.post(
+        "/login", data={"username": test_user["email"], "password": "wrong_password"}
+    )
+
+    assert res.status_code == 403
+    assert res.json().get("detail") == "Invalid Credentials"
