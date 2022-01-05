b38102cfa74074c31da8268bf34ba8b54c13c03b
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-05 06:48:15 +0100

    completed login test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,8 @@
+import pytest
+from jose import jwt
+
 from app import schemas
+from app.config import settings
 from .database import client, session
 
 #
@@ -6,11 +10,16 @@ from .database import client, session
 #
 
 
-def test_root(client):
-    response = client.get("/")
+@pytest.fixture()
+def test_user(client):
+    user_data = {"email": "hello123@pytest.de", "password": "password123"}
 
-    assert response.json().get("message") == "Hello, new stuff"
-    assert response.status_code == 200
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user["password"] = user_data["password"]
+    return new_user
 
 
 def test_create_user(client):
@@ -21,3 +30,20 @@ def test_create_user(client):
 
     assert new_user.email == "hello123@pytest.de"
     assert res.status_code == 201
+
+
+def test_login_user(client, test_user):
+    res = client.post(
+        "/login",
+        data={"username": test_user["email"], "password": test_user["password"]},
+    )
+
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(
+        login_res.access_token, settings.secret_key, algorithms=[settings.algorithm]
+    )
+    id = payload["user_id"]
+
+    assert id == test_user["id"]
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
